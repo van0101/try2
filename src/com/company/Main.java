013514835d9621a385a40b6bd1dea1319d36912e
Vandit Sharma <van0101@users.noreply.github.com>
2020-05-12 18:10:47 +0530

    Removed comments and improved intendation

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,12 +4,7 @@ public class Main {
 
     public static void main(String[] args)
     {
-//        System.out.println("Hello World!");
-//        System.out.println("Hello Git!");
-//        System.out.println("I am V");
-//        System.out.println("Branch changes here");
-	    
-	 System.out.println("Change from github for pull");
-	System.out.println("Bye git!");
+	    System.out.println("Change from github for pull");
+	    System.out.println("Bye git!");
     }
 }
