ebd534c4a478d87254be78df34d7a423ac8bab69
Vandit Sharma <van0101@users.noreply.github.com>
2020-05-12 12:17:47 +0530

    Commit 2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,7 +2,9 @@ package com.company;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World!");
 	// write your code here
     }
 }
