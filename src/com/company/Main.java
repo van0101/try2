991762e879917268f39b3e801881631024d5f968
Vandit Sharma <van0101@users.noreply.github.com>
2020-05-12 13:17:40 +0530

    Bye git added

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,12 +4,12 @@ public class Main {
 
     public static void main(String[] args)
     {
-        System.out.println("Hello World!");
-        System.out.println("Hello Git!");
-        System.out.println("I am V");
-        System.out.println("Branch changes here");
+//        System.out.println("Hello World!");
+//        System.out.println("Hello Git!");
+//        System.out.println("I am V");
+//        System.out.println("Branch changes here");
 	    
 	 System.out.println("Change from github for pull");
-	// write your code here
+	System.out.println("Bye git!");
     }
 }
